c3fcfb341b0e7fab292d06e32a38d06301e57514
gaymemohamed <gaymemohamed@gmail.com>
2017-11-16 15:06:16 +0200

    Add files via upload

---
+++ Tic tac tok/dist/js/main.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+
+    var o = 'o';
+    var x = 'x';
+    var turns = 0;
+    var spot1 = $('.spot1');
+    var spot2 = $('.spot2');
+    var spot3 = $('.spot3');
+    var spot4 = $('.spot4');
+    var spot5 = $('.spot5');
+    var spot6 = $('.spot6');
+    var spot7 = $('.spot7');
+    var spot8 = $('.spot8');
+    var spot9 = $('.spot9');
+    
+    $('.header li').click(function(){
+        if(spot1.hasClass('o') &&spot2.hasClass('o') &&spot3.hasClass('o') || spot4.hasClass('o') &&spot5.hasClass('o') &&spot6.hasClass('o') || spot7.hasClass('o') &&spot8.hasClass('o') &&spot9.hasClass('o') || spot1.hasClass('o') &&spot3.hasClass('o') &&spot9.hasClass('o') || spot1.hasClass('o') &&spot4.hasClass('o') &&spot7.hasClass('o') || spot2.hasClass('o') &&spot5.hasClass('o') &&spot8.hasClass('o') || spot3.hasClass('o') &&spot6.hasClass('o') &&spot9.hasClass('o') ||spot3.hasClass('o') &&spot5.hasClass('o') &&spot7.hasClass('o')){
+            alert('winner: o');
+            $('.header li').text('+');
+            $('.header li').removeClass('disable');
+            $('.header li').removeClass('o');
+            $('.header li').removeClass('x');
+        }else if(spot1.hasClass('x') &&spot2.hasClass('x') &&spot3.hasClass('x') || spot4.hasClass('x') &&spot5.hasClass('x') &&spot6.hasClass('x') || spot7.hasClass('x') &&spot8.hasClass('x') &&spot9.hasClass('x') || spot1.hasClass('x') &&spot3.hasClass('x') &&spot9.hasClass('x') || spot1.hasClass('x') &&spot4.hasClass('x') &&spot7.hasClass('x') || spot2.hasClass('x') &&spot5.hasClass('x') &&spot8.hasClass('x') || spot3.hasClass('x') &&spot6.hasClass('x') &&spot9.hasClass('x') ||spot3.hasClass('x') &&spot5.hasClass('x') &&spot7.hasClass('x')){
+            alert('winner: x');
+            $('.header li').text('+');
+            $('.header li').removeClass('disable');
+            $('.header li').removeClass('o');
+            $('.header li').removeClass('x');
+        }else if(turns == 9){
+            alert('tie game');
+            $('.header li').text('+');
+            $('.header li').removeClass('disable');
+            $('.header li').removeClass('o');
+            $('.header li').removeClass('x');
+            turns = 0;
+        }else if($(this).hasClass('disable')){
+            alert('this spot has already filled')
+        }else if(turns % 2 == 0){
+            turns++;
+            $(this).text(o);
+            $(this).addClass('disable o');
+            if(spot1.hasClass('o') &&spot2.hasClass('o') &&spot3.hasClass('o') || spot4.hasClass('o') &&spot5.hasClass('o') &&spot6.hasClass('o') || spot7.hasClass('o') &&spot8.hasClass('o') &&spot9.hasClass('o') || spot1.hasClass('o') &&spot3.hasClass('o') &&spot9.hasClass('o') || spot1.hasClass('o') &&spot4.hasClass('o') &&spot7.hasClass('o') || spot2.hasClass('o') &&spot5.hasClass('o') &&spot8.hasClass('o') || spot3.hasClass('o') &&spot6.hasClass('o') &&spot9.hasClass('o') ||spot3.hasClass('o') &&spot5.hasClass('o') &&spot7.hasClass('o')){
+            alert('winner: o');
+            turns = 0;
+            }
+        }else{
+                turns++;
+                $(this).text(x);
+                $(this).addClass('x');
+                if(spot1.hasClass('x') &&spot2.hasClass('x') &&spot3.hasClass('x') || spot4.hasClass('x') &&spot5.hasClass('x') &&spot6.hasClass('x') || spot7.hasClass('x') &&spot8.hasClass('x') &&spot9.hasClass('x') || spot1.hasClass('x') &&spot3.hasClass('x') &&spot9.hasClass('x') || spot1.hasClass('x') &&spot4.hasClass('x') &&spot7.hasClass('x') || spot2.hasClass('x') &&spot5.hasClass('x') &&spot8.hasClass('x') || spot3.hasClass('x') &&spot6.hasClass('x') &&spot9.hasClass('x') ||spot3.hasClass('x') &&spot5.hasClass('x') &&spot7.hasClass('x')){
+            alert('winner: x');
+            turns = 0;
+            }
+        }
+        
+        $('.reset').click(function(){
+            $('.header li').text('+');
+            $('.header li').removeClass('disable');
+            $('.header li').removeClass('o');
+            $('.header li').removeClass('x');
+            turns = 0;
+        })
+    })
+        
+    
+})
